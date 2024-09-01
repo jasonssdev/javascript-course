70f27bcd89dac28a1055b9506ad40f83a43d6826
Jasonssdev <jasonssdev@gmail.com>
2024-09-01 18:09:57 -0500

    operators

---
+++ operators.js
@@ -0,0 +1,37 @@
+// Comparison
+
+const a = 10
+const b = 20
+const c = '10'
+
+console.log(a == c)
+console.log(a === c)
+console.log(a > b)
+console.log(a < b)
+console.log(a >= b)
+console.log(a <= b)
+console.log(a != c)
+console.log(a !== c)
+
+// Logic
+
+let isProducer = true
+let isDj = false
+let isMusiciam = true
+// And
+console.log(isProducer && isMusiciam)
+console.log(isProducer && isDj)
+// Or
+console.log(isDj || isProducer)
+console.log(isDj || isMusiciam)
+// Not
+console.log(!(isDj))
+console.log(!(isProducer))
+
+
+
+
+
+
+
+
