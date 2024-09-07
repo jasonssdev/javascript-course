68765c533ebf9dc2be170858762e1a77717626eb
Jasonssdev <jasonssdev@gmail.com>
2024-09-07 17:06:12 -0500

    singleton

--- design-patterns/01-sigleton/singleton.js
+++ design-patterns/01-sigleton/singleton.js
@@ -1,8 +1,36 @@
 /* 
 
 - How to implement Singleton patter?
-1. Make the constructor privarte
-2. Crreate a static method who calls the private constructor and sabe the intance in a static variable
+1. Make the constructor private
+2. Create a static method who calls the private constructor and sabe the intance in a static variable
 
 */ 
 
+class Singleton {
+    static instance = undefined
+
+    constructor(version) {
+        this.version = version
+
+    }
+
+    static getInstance(version) {
+        if(!Singleton.instance) {
+            Singleton.instance = new Singleton(version)
+        }
+        return Singleton.instance
+    }
+}
+
+function appSingleton() {
+    const singleton1 = Singleton.getInstance("version-1")
+    const singleton2 = Singleton.getInstance("version-2")
+    const singleton3 = Singleton.getInstance("version-3")
+
+    console.log(singleton1 === singleton2)
+    console.log(singleton1 === singleton3)
+
+
+}
+
+appSingleton()
