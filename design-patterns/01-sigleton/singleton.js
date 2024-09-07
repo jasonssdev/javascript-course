8105868ced4efc863f5ff649858319528c304d17
Jasonssdev <jasonssdev@gmail.com>
2024-09-07 16:14:35 -0500

    new structure

---
+++ design-patterns/01-sigleton/singleton.js
@@ -0,0 +1,8 @@
+/* 
+
+- How to implement Singleton patter?
+1. Make the constructor privarte
+2. Crreate a static method who calls the private constructor and sabe the intance in a static variable
+
+*/ 
+
